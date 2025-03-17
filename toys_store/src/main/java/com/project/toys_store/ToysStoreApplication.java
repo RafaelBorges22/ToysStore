40ab28215fd71ed56dc5dc3d78b688cfdd46570b
RafaelBorges22 <rafaelmascarenhasborges@gmail.com>
2025-03-17 19:26:05 -0300

    First commit :rocket:

---
+++ toys_store/src/main/java/com/project/toys_store/ToysStoreApplication.java
@@ -0,0 +1,13 @@
+package com.project.toys_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToysStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToysStoreApplication.class, args);
+	}
+
+}
